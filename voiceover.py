67fd2d25d2a094f9811cc7c035e8e4a9c83048fd
emmittjames <emmittjames1@gmail.com>
2023-12-23 14:48:07 -0500

    TikTok tts working

--- voiceover.py
+++ voiceover.py
@@ -21,3 +21,21 @@ def create_voice_over(fileName, text):
     tts = gTTS(text=text, lang='en')
     tts.save(filePath)
     return filePath
+
+
+"""
+from tiktok_tts import main as tts
+
+voiceoverDir = "Voiceovers"
+
+def create_voice_over(fileName, text):
+    # Set your TikTok session ID and other parameters
+    session_id = "57b7d8b3e04228a24cc1e6d25387603a"
+    voice_code = "en_us_002"
+    filePath = f"{voiceoverDir}/{fileName}.mp3"
+
+    # Call the main function with the specified parameters
+    tts(["-v", voice_code, "-f", "testtext.txt", "-s", session_id, "-n", filePath])
+
+create_voice_over("test", "This is a test")
+"""
