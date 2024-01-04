ca3f7b8d4afe3cd641995e1cd944a197e48eeed3
emmittjames <emmittjames1@gmail.com>
2024-01-04 13:15:02 -0500

    Added more voices

--- voiceover.py
+++ voiceover.py
@@ -27,22 +27,26 @@ import tiktok_tts, random, subprocess
 
 voiceoverDir = "Voiceovers"
 
-"""
-voices = [
-    'en_us_001', 'en_us_006', 'en_us_007', 'en_us_009'
-]
-"""
+# voices = ['en_us_001', 'en_us_006', 'en_us_007', 'en_us_009']
 
-voices = ['en_us_006']
+# voices = ['en_us_006']
+# voices = ['en_male_funny']
+
+voices = [  
+            'en_au_001', 'en_au_002', 'en_uk_001', 'en_uk_003', 'en_us_001', 'en_us_006', 'en_us_007', 'en_us_009', 'en_us_010', 
+            'en_male_funny', 'en_male_pirate', 'en_female_madam_leota', 'en_us_rocket'
+            ]
+
+# voices = ['en_us_rocket', 'en_male_pirate', 'en_female_madam_leota']
 
 def create_voice_over(fileName, script_path):
-    random_voice = random.choice(voices)
+    voice = random.choice(voices)
     file_path = f"{voiceoverDir}/{fileName}.mp3"
 
     command = [
         'python3',
         'tiktok_tts.py',
-        '-v', random_voice,
+        '-v', voice,
         '-f', script_path,
         '-n', file_path,
         '--session', '00453c9bbd7ea65b290bfa2656f7bd08',
