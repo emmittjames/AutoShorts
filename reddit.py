b46b0a9ff5c8007f1530c55a69526a0812de9efe
emmittjames <emmittjames1@gmail.com>
2024-01-27 16:46:18 -0500

    Added filtering to post selection based on word count and paragraphs

--- reddit.py
+++ reddit.py
@@ -40,7 +40,10 @@ def getContent(outputDir, postOptionCount) -> VideoScript:
         hoursAgoPosted = (now - submission.created_utc) / 3600
         paragraph_count = submission.selftext.count('\n') + 1
         word_count = len(submission.selftext.split())
-        print(f"[{len(posts)}] {submission.title} | Word Count: {word_count} | Paragraph Count: {paragraph_count} | {'{:.1f}'.format(hoursAgoPosted)} hours ago")
+        word_to_paragraph_ratio = word_count / paragraph_count
+        if (word_count > 250 or word_to_paragraph_ratio>70):
+            continue
+        print(f"[{len(posts)}] {submission.title} | Word Count: {word_count} | Paragraph Count: {paragraph_count} | Upvotes: {submission.score} | {'{:.1f}'.format(hoursAgoPosted)} hours ago")
         posts.append(submission)
         if (len(posts) >= postOptionCount):
             break
